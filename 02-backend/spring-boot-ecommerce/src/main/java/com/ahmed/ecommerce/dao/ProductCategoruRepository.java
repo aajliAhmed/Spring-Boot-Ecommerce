4744f7da63ac02a1a4dae804fd51a427df24ebe0
AAJLI Ahmed <ahmed.conceptor@gmail.com>
2024-02-25 14:19:28 +0100

    devlop productCategoryRepository

--- 02-backend/spring-boot-ecommerce/src/main/java/com/ahmed/ecommerce/dao/ProductCategoruRepository.java
+++ 02-backend/spring-boot-ecommerce/src/main/java/com/ahmed/ecommerce/dao/ProductCategoruRepository.java
@@ -1,4 +1,9 @@
 package com.ahmed.ecommerce.dao;
 
-public interface ProductCategoruRepository {
+import com.ahmed.ecommerce.entity.ProductCategory;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+@RepositoryRestResource(collectionResourceRel = "productCategory",path = "product-category")
+public interface ProductCategoruRepository extends JpaRepository<ProductCategory,Long> {
 }
