f41d8336a2be3c4cb3ddd71c7ede5b5ad913ab88
AAJLI Ahmed <ahmed.conceptor@gmail.com>
2024-02-26 14:42:18 +0100

    devlop class configurartion

--- 02-backend/spring-boot-ecommerce/src/main/java/com/ahmed/ecommerce/config/MyDataRestConfig.java
+++ 02-backend/spring-boot-ecommerce/src/main/java/com/ahmed/ecommerce/config/MyDataRestConfig.java
@@ -1,4 +1,14 @@
 package com.ahmed.ecommerce.config;
 
-public class MyDataRestConfig {
+import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
+import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
+import org.springframework.http.HttpMethod;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+
+public class MyDataRestConfig implements RepositoryRestConfigurer {
+    @Override
+    public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config, CorsRegistry cors) {
+        RepositoryRestConfigurer.super.configureRepositoryRestConfiguration(config, cors);
+        HttpMethod[] theUnsupportedActions = {HttpMethod.PUT,HttpMethod.DELETE,HttpMethod.POST};
+    }
 }
