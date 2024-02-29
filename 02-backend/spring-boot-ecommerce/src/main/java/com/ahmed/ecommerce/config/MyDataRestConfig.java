f02e30414e40310ded55711d9a737090ecbf68de
AAJLI Ahmed <ahmed.conceptor@gmail.com>
2024-02-29 14:56:01 +0100

    add annotation of configuration

--- 02-backend/spring-boot-ecommerce/src/main/java/com/ahmed/ecommerce/config/MyDataRestConfig.java
+++ 02-backend/spring-boot-ecommerce/src/main/java/com/ahmed/ecommerce/config/MyDataRestConfig.java
@@ -2,11 +2,13 @@ package com.ahmed.ecommerce.config;
 
 import com.ahmed.ecommerce.entity.Product;
 import com.ahmed.ecommerce.entity.ProductCategory;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
 import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
 import org.springframework.http.HttpMethod;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 
+@Configuration
 public class MyDataRestConfig implements RepositoryRestConfigurer {
     @Override
     public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config, CorsRegistry cors) {
